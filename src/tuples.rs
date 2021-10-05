4ace3883ff273f28979a482a5ce52179ececdfc0
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-10-06 00:23:04 +0800

    Add scalar division

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,6 +1,6 @@
 use crate::math::Real;
 use std::ops;
-use std::ops::{Add, Sub, Neg, Mul};
+use std::ops::{Add, Sub, Neg, Mul, Div};
 
 #[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Tuple {
@@ -64,6 +64,14 @@ impl ops::Mul<Real> for Tuple {
 
     fn mul(self, scalar: Real) -> Self::Output {
         Tuple::new(self.x * scalar, self.y * scalar, self.z * scalar, self.w * scalar)
+    }
+}
+
+impl ops::Div<Real> for Tuple {
+    type Output = Tuple;
+
+    fn div(self, scalar: Real) -> Self::Output {
+        self * (1.0 / scalar)
     }
 }
 
@@ -166,5 +174,11 @@ mod tests {
     fn test_multiply_by_fraction() {
         let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
         assert_eq!(tuple * 0.5, Tuple::new(0.5, -1.0, 1.5, -2.0));
+    }
+
+    #[test]
+    fn test_scalar_division() {
+        let tuple = Tuple::new(1.0, -2.0, 3.0, -4.0);
+        assert_eq!(tuple / 2.0, Tuple::new(0.5, -1.0, 1.5, -2.0));
     }
 }
