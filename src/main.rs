047e3150995d42e079561c688c7e598ae1fbd8fd
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-10-24 08:56:45 +0800

    Add rendering execution time

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 mod tuples;
 mod math;
 mod canvas;
@@ -11,5 +13,12 @@ mod scene;
 mod camera;
 
 fn main() {
+    println!("Rendering...");
+    let start = SystemTime::now()
+        .duration_since(UNIX_EPOCH).expect("Time isn't working today");
     renderer::render_scene();
+    let end = SystemTime::now()
+        .duration_since(UNIX_EPOCH).expect("Time is still on leave");
+
+    println!("Render complete. Execution time: {:?} seconds", end.as_secs() - start.as_secs());
 }
