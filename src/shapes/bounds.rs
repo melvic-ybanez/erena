ca7ba8f87dac78ecd60c259cf4847a602267f1bf
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-11-13 12:29:11 +0800

    Add support for adding points to box

--- src/shapes/bounds.rs
+++ src/shapes/bounds.rs
@@ -3,6 +3,7 @@ use crate::shapes::{Shape, Geo};
 use crate::tuples::points;
 use crate::math::Real;
 use crate::shapes::cylinders::CylLike;
+use std::ops::Add;
 
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Bounds {
@@ -42,6 +43,25 @@ impl Bounds {
     }
 }
 
+impl Add<Point> for Bounds {
+    type Output = Bounds;
+
+    fn add(self, rhs: Point) -> Self::Output {
+        Bounds::new(
+            points::new(
+                Real::min(self.min.x, rhs.x),
+                Real::min(self.min.y, rhs.y),
+                Real::min(self.min.z, rhs.z),
+            ),
+            points::new(
+                Real::max(self.max.x, rhs.x),
+                Real::max(self.max.y, rhs.y),
+                Real::max(self.max.z, rhs.z),
+            ),
+        )
+    }
+}
+
 // TODO: Add more bounding box tests here.
 #[cfg(test)]
 mod tests {
@@ -56,7 +76,17 @@ mod tests {
         let min = points::new(Real::INFINITY, Real::INFINITY, Real::INFINITY);
         let max = (-min).to_point();
         assert_eq!(bbox.min, min);
-        //assert_eq!(bbox.max, max);
+        assert_eq!(bbox.max, max);
+    }
+
+    #[test]
+    fn test_adding_points_to_box() {
+        let bbox = Bounds::empty();
+        let p1 = points::new(-5.0, 2.0, 0.0);
+        let p2 = points::new(7.0, 0.0, -3.0);
+        let bbox = bbox + p1 + p2;
+        assert_eq!(bbox.min, points::new(-5.0, 0.0, -3.0));
+        assert_eq!(bbox.max, points::new(7.0, 2.0, 0.0));
     }
 
     #[test]
