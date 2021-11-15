aa627be716a537eb8886383436eaaa9c295a3bcb
Melvic Ybanez <melvic.ybanez2014@gmail.com>
2021-11-16 04:20:32 +0800

    Simplify and fix test rand

--- src/math/random.rs
+++ src/math/random.rs
@@ -1,51 +1,42 @@
 use std::cell::{RefCell, RefMut};
 use std::slice::Iter;
 
-pub trait Random<'a> {
-    fn next(&'a self) -> f64;
+pub trait Random {
+    fn next(&mut self) -> f64;
 }
 
-pub struct TestRand<'a> {
+pub struct TestRand {
     seq: Vec<f64>,
-    iter: RefCell<Option<Iter<'a, f64>>>,
+    current_index: usize,
 }
 
-impl<'a> TestRand<'a> {
-    pub fn from_seq(seq: Vec<f64>) -> TestRand<'a> {
+impl TestRand {
+    pub fn from_seq(seq: Vec<f64>) -> TestRand {
         TestRand {
             seq,
-            iter: RefCell::new(None),
+            current_index: 0,
         }
     }
 
-    pub fn maybe_next(&'a self) -> Option<f64> {
-        let mut maybe_iter = self.iter.borrow_mut();
-        let mut reset = |mut maybe_iter: RefMut<Option<Iter<'a, f64>>>| {
-            let mut new_iter = self.seq.iter();
-            let result = new_iter.next().cloned();
-
-            // reset iterator to make the set of options cyclic
-            *maybe_iter = Some(new_iter);
-
-            // return the next item again
+    pub fn maybe_next(&mut self) -> Option<f64> {
+        if self.seq.is_empty() {
+            None
+        } else {
+            let result = Some(self.seq[self.current_index % self.seq.len()]);
+            self.current_index += 1;
             result
-        };
-
-        match maybe_iter.clone() {
-            None => reset(maybe_iter),
-            Some(mut iter) => iter.next().cloned().or_else(|| reset(maybe_iter)),
         }
     }
 }
 
-impl<'a> Random<'a> for f64 {
-    fn next(&'a self) -> f64 {
+impl Random for f64 {
+    fn next(&mut self) -> f64 {
         rand::random()
     }
 }
 
-impl<'a> Random<'a> for TestRand<'a> {
-    fn next(&'a self) -> f64 {
+impl Random for TestRand {
+    fn next(&mut self) -> f64 {
         // return the next item. Crash the program if
         // iterator still can't return anything
         self.maybe_next().expect("Can't fetch next value")
@@ -60,7 +51,7 @@ mod tests {
     /// a cyclic sequence of numbers
     #[test]
     fn test_random_cyclic_sequence() {
-        let gen = TestRand::from_seq(vec![0.1, 0.5, 1.0]);
+        let mut gen = TestRand::from_seq(vec![0.1, 0.5, 1.0]);
         assert_eq!(gen.next(), 0.1);
         assert_eq!(gen.next(), 0.5);
         assert_eq!(gen.next(), 1.0);
